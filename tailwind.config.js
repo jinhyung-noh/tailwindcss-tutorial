03f0e8a427fd2135f4cecb49e2890165af316bcd
jinhyung.noh <jinhyung.noh@voyagerx.com>
2022-09-29 23:42:20 +0900

    ~ lesson 11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js"],
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
   },
   plugins: [],
 };
